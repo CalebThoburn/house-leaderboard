691c102406de5f66e2f27508eddbb2b4b33675e0
CalebThoburn <calebthoburn4@gmail.com>
2026-05-11 23:19:43 -0700

    stuff

--- script.js
+++ script.js
@@ -188,7 +188,7 @@ function initQuotes() {
     const current = createQuoteElement(quotes[quoteIndex], "quote-item quote-center");
     container.appendChild(current);
 
-    setInterval(nextQuote, 1200);
+    setInterval(nextQuote, 700);
 }
 
 function createQuoteElement(quote, className) {
